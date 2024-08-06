58a86f5a5aba23d395fab0b0140ecc668f3bf7a9
OmniTechnoMancer <OmniTechnoMancer@wah.quest>
2024-08-07 03:15:27 +1200

    Fix typo in assembler.rs and print out diagnostics on error

--- src/assembler.rs
+++ src/assembler.rs
@@ -2,19 +2,20 @@ use customasm::{asm, diagn, util};
 
 pub fn assemble(asm: &str) -> Result<Vec<u8>, ()> {
 
-  let mut fielserver = util::FileServerMock::new();
+  let mut fileserver = util::FileServerMock::new();
 
   let asm = String::from("#include \"fox.asm\"\n\n") + asm;
 
-  fielserver.add_std_files(&[("fox.asm",include_str!("../assembler/fox.asm")), ("input", &asm)]);
+  fileserver.add_std_files(&[("fox.asm",include_str!("../assembler/fox.asm")), ("input", &asm)]);
 
   let mut report = diagn::Report::new();
   let opts = asm::AssemblyOptions::new();
 
-  let result = asm::assemble(&mut report, &opts, &mut fielserver, &["input"]);
+  let result = asm::assemble(&mut report, &opts, &mut fileserver, &["input"]);
 
   if result.error
   {
+    report.print_all(&mut std::io::stderr(), &fileserver, true);
     return Err(());
   }
 
