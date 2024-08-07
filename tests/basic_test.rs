d1d19c0b7b7e080e23ebc3e56dbe1fcf704769df
OmniTechnoMancer <OmniTechnoMancer@wah.quest>
2024-08-08 03:55:01 +1200

    Add logic operation tests
    Add some tests for existing logic operations to confirm they still work
    after instruction format conversion to new ISA.

--- tests/basic_test.rs
+++ tests/basic_test.rs
@@ -44,6 +44,66 @@ fn test_sub() {
 }
 
 #[test]
+fn test_and() {
+  let mut foxmulator = Foxmulator::singleton().unwrap();
+
+  foxmulator.state.r[0] = 0b1010;
+  foxmulator.state.r[1] = 0b1100;
+  foxmulator.run_assembly(r#"
+    block 2
+    and r0, r1
+    halt
+  "#);
+
+  assert_eq!(foxmulator.state.r[0], 0b1000);
+}
+
+#[test]
+fn test_or() {
+  let mut foxmulator = Foxmulator::singleton().unwrap();
+
+  foxmulator.state.r[0] = 0b1010;
+  foxmulator.state.r[1] = 0b1100;
+  foxmulator.run_assembly(r#"
+    block 2
+    or r0, r1
+    halt
+  "#);
+
+  assert_eq!(foxmulator.state.r[0], 0b1110);
+}
+
+#[test]
+fn test_xor() {
+  let mut foxmulator = Foxmulator::singleton().unwrap();
+
+  foxmulator.state.r[0] = 0b1010;
+  foxmulator.state.r[1] = 0b1100;
+  foxmulator.run_assembly(r#"
+    block 2
+    xor r0, r1
+    halt
+  "#);
+
+  assert_eq!(foxmulator.state.r[0], 0b0110);
+}
+
+#[test]
+fn test_andc() {
+  let mut foxmulator = Foxmulator::singleton().unwrap();
+
+  foxmulator.state.r[0] = 0b1010;
+  foxmulator.state.r[1] = 0b1100;
+  foxmulator.run_assembly(r#"
+    block 2
+    andc r0, r1
+    halt
+  "#);
+
+  assert_eq!(foxmulator.state.r[0], 0b0010);
+}
+
+#[test]
 fn test_b() {
   let mut foxmulator = Foxmulator::singleton().unwrap();
 
