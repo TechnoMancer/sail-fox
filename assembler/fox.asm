a2df34ea4de642351702f6af0a763f6121db8623
OmniTechnoMancer <OmniTechnoMancer@wah.quest>
2024-08-16 04:38:56 +1200

    Change assembly for predicated b and call to be spelled with 'if'

--- assembler/fox.asm
+++ assembler/fox.asm
@@ -210,11 +210,11 @@ set {rd: register}, {val: i16} => {
 ; | 0000 0010 dddd aaaa | neg rd, ra
   neg {rd: register}, {ra: register} => 0b0000_0010 @ rd @ ra
 ; | 0000 1011 0ddd pppp | b td (predicated)
-  b {td: target} ({p: predicate}) => 0b0000_1011 @ 0b0 @ td`3 @ p`4
+  b {td: target} if {p: predicate} => 0b0000_1011 @ 0b0 @ td`3 @ p`4
 ; short for always branch
   b {td: target} => 0b0000_1011 @ 0b0 @ td`3 @ 0b0111
 ; | 0000 1011 1ddd pppp | call td (predicated)
-  call {td: target} ({p: predicate}) => 0b0000_1011 @ 0b1 @ td`3 @ p`4
+  call {td: target} if {p: predicate} => 0b0000_1011 @ 0b1 @ td`3 @ p`4
 ; short for always branch
   call {td: target} => 0b0000_1011 @ 0b1 @ td`3 @ 0b0111
 ; | 0001 0000 dddd aaaa | and rd, ra
